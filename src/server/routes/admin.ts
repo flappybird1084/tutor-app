5744e97a15c7030045bea090363a53ca0bb47c78
Redger Xu <redgerxu@gmail.com>
2025-05-13 17:26:09 -0700

    refactor adminOnly middleware

--- src/server/routes/admin.ts
+++ src/server/routes/admin.ts
@@ -14,7 +14,9 @@ export const adminOnly = (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-adminRouter.get('/mainpage', adminOnly, async (req: Request, res: Response) => {
+adminRouter.use('/', adminOnly);
+
+adminRouter.get('/mainpage', async (req: Request, res: Response) => {
   // console.log('User is authenticated:', req.user);
   const userObject = await User.findById(req.user.sub);
   const email = userObject?.email;
@@ -23,46 +25,33 @@ adminRouter.get('/mainpage', adminOnly, async (req: Request, res: Response) => {
   res.render('admin/admin', { user: req.user, email, username, role });
 });
 
-adminRouter.get('/users', adminOnly, async (req: Request, res: Response) => {
+adminRouter.get('/users', async (req: Request, res: Response) => {
   const users = await User.find();
   res.render('admin/user-management', { users });
 });
 
-adminRouter.get(
-  '/assignments',
-  adminOnly,
-  async (req: Request, res: Response) => {
-    const assignments = await Assignment.find();
-    res.render('admin/assignments/assignments', { assignments });
-    res.redirect('/auth/login');
-  }
-);
+adminRouter.get('/assignments', async (req: Request, res: Response) => {
+  const assignments = await Assignment.find();
+  res.render('admin/assignments/assignments', { assignments });
+  res.redirect('/auth/login');
+});
 
-adminRouter.get(
-  '/assignments/create',
-  adminOnly,
-  async (req: Request, res: Response) => {
-    res.render('admin/assignments/create-assignment');
-  }
-);
+adminRouter.get('/assignments/create', async (req: Request, res: Response) => {
+  res.render('admin/assignments/create-assignment');
+});
 
-adminRouter.post(
-  '/assignments/create',
-  adminOnly,
-  async (req: Request, res: Response) => {
-    const { dueDate } = req.body;
-    const title = req.body.title;
-    const description = req.body.description;
-    // const dueDate = req.body.dueDate;
-    const assignment = new Assignment({ title, description });
-    await assignment.save();
-    res.redirect('/admin/assignments');
-  }
-);
+adminRouter.post('/assignments/create', async (req: Request, res: Response) => {
+  const { dueDate } = req.body;
+  const title = req.body.title;
+  const description = req.body.description;
+  // const dueDate = req.body.dueDate;
+  const assignment = new Assignment({ title, description });
+  await assignment.save();
+  res.redirect('/admin/assignments');
+});
 
 adminRouter.get(
   '/assignments/edit/:id',
-  adminOnly,
   async (req: Request, res: Response) => {
     const assignment = await Assignment.findById(req.params.id);
     if (!assignment) {
@@ -74,7 +63,6 @@ adminRouter.get(
 
 adminRouter.post(
   '/assignments/edit/:id',
-  adminOnly,
   async (req: Request, res: Response) => {
     const { dueDate } = req.body;
     const title = req.body.title;
@@ -96,7 +84,6 @@ adminRouter.post(
 
 adminRouter.delete(
   '/assignments/delete/:id',
-  adminOnly,
   async (req: Request, res: Response) => {
     const assignment = await Assignment.findById(req.params.id);
     if (!assignment) {
